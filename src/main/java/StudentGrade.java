c6ff4491cfd37bc8112ffab7bf9457f3c3cc8595
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2025-03-19 19:24:34 +0200

    Added a method to calculate sunmlist

--- src/main/java/StudentGrade.java
+++ src/main/java/StudentGrade.java
@@ -2,7 +2,6 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class StudentGrade {
-
         public  static  void main(String[] args){
             ArrayList<Integer> grades = new ArrayList<>();
             Integer gradeCount = 0;
@@ -15,22 +14,26 @@ public class StudentGrade {
                 // adding the new Grade to the grades list
                 grades.add(newGrade );
                 if(grades.size() ==  5){
-
-
+                    var average = sumList(grades) / grades.size();
+                    System.out.println"Average is :" +(average);
                     break;
                 }else {
                     continue;
                 }
+            }
+        }
 
+    /**
+     * Method to calculate the sum of the list
+     * @param list
+     * @return
+     */
+    static  int sumList(ArrayList<Integer> list){
+            int sum = 0;
+            for(int i= 0; i < list.size() ; i ++){
+                sum += list.get(i);
 
             }
-
-
+            return  sum;
         }
-        void printAverage(ArrayList<Integer> list){
-
-            var average = list.
-
-        }
-
 }
